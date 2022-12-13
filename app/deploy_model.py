7b535e0080296fc85583a736b6b3feb07d8acc89
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2022-12-13 23:46:06 +0100

    fg

--- app/deploy_model.py
+++ app/deploy_model.py
@@ -6,11 +6,42 @@ print("ej 1")
 houses = pd.read_csv("regression_data.csv")
 print(houses.head())
 
+fila = ["id","date","bedrooms","bathrooms","sqft_living","sqft_lot",
+        "floors","waterfront","view","condition","grade","sqft_above",
+        "sqft_basement","yr_built","yr_renovated","zipcode","lat","long",
+        "sqft_living15","sqft_lot15","price"]
+x = houses.columns
+houses.columns = fila
+houses.loc[0] = x
+print(houses.head())
 
 
+print("ej 2")
+
+houses.drop(["date"],axis=1,inplace = True)
+print(houses.head())
+
+print("ej 3")
+
+print(len(houses.axes[0]))
+
+print("ej 4")
+
+print(houses["bedrooms"].unique())
+print(houses["bathrooms"].unique())
+print(houses["floors"].unique())
+print(houses["condition"].unique())
+print(houses["grade"].unique())
+
+
+
+print("ej 5")
+
+a =houses.sort_values(houses["price"],ascending= False)
+print(a.head())
 
 
 print("ej 6")
 
-houses["price"].mean()
+print(houses["price"].mean())
 
